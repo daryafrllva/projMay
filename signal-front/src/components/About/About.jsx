6e830dfc06e6612b4bc57bd5c15a40e52eb8f4c9
daryafrllva <dariyf995@gmail.com>
2025-05-01 12:32:27 +0700

    исправлю позже

--- signal-front/src/components/About/About.jsx
+++ signal-front/src/components/About/About.jsx
@@ -34,10 +34,11 @@ const About = () => {
             <div className="about-hero">
                 <div className="about-hero-content">
                     <h1 className="about-hero-title">
-                        Мы анализируем<br />
-                        Создаем стратегии<br />
-                        Достигаем успеха
-                    </h1>
+                            <span>Мы анализируем</span>
+                            <span>Создаем стратегии</span>
+                            <span>Достигаем успеха</span>
+                        </h1>
+
                 </div>
             </div>
 
@@ -48,30 +49,30 @@ const About = () => {
 
                 <div className="about-scroll-content">
                     <div className="text-sections-wrapper">
-                    <div className="about-image-section">
-                        <img src={bar} alt="background" className="about-main-image" />
+                        <div className="about-image-section">
+                            <img src={bar} alt="background" className="about-main-image" />
+                        </div>
+
+                        <div className="text-sections-container">
+                            <section className="about-section" ref={el => sectionsRef.current[0] = el}>
+                                <div className="about-section-text">
+                                    <p>Мы меняем трейдинг, делая его простым, понятным и доступным. Вы ставите цели, а мы предоставляем инструменты, которые ведут к успеху.</p>
+                                </div>
+                            </section>
+
+                            <section className="about-section" ref={el => sectionsRef.current[1] = el}>
+                                <div className="about-section-text">
+                                    <p>Технологии и аналитика работают на вас. Мы создаем решения, которые снижают риски и максимизируют прибыль в каждой сделке.</p>
+                                </div>
+                            </section>
+
+                            <section className="about-section" ref={el => sectionsRef.current[2] = el}>
+                                <div className="about-section-text">
+                                    <p>Трейдинг – это ваша история, и мы делаем ее ярче. Удобный интерфейс, передовая аналитика и полная поддержка на каждом этапе – все это помогает вам достигать вершин.</p>
+                                </div>
+                            </section>
+                        </div>
                     </div>
-
-                    <div className="text-sections-container">
-                    <section className="about-section" ref={el => sectionsRef.current[0] = el}>
-                        <div className="about-section-text">
-                            <p>Мы меняем трейдинг, делая его простым, понятным и доступным. Вы ставите цели, а мы предоставляем инструменты, которые ведут к успеху.</p>
-                        </div>
-                    </section>
-
-                    <section className="about-section" ref={el => sectionsRef.current[1] = el}>
-                        <div className="about-section-text">
-                            <p>Технологии и аналитика работают на вас. Мы создаем решения, которые снижают риски и максимизируют прибыль в каждой сделке.</p>
-                        </div>
-                    </section>
-
-                    <section className="about-section" ref={el => sectionsRef.current[2] = el}>
-                        <div className="about-section-text">
-                            <p>Трейдинг – это ваша история, и мы делаем ее ярче. Удобный интерфейс, передовая аналитика и полная поддержка на каждом этапе – все это помогает вам достигать вершин.</p>
-                        </div>
-                    </section>
-                    </div>
-                </div>
                 </div>
             </div>
 
